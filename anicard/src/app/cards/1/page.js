00b21e34d214bd3cbe16a9dd162d7cd8320f2856
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2023-11-10 22:47:38 -0300

    Update vertical card colors

--- anicard/src/app/cards/1/page.js
+++ anicard/src/app/cards/1/page.js
@@ -6,48 +6,64 @@ Vertical card with poster.
 
 import { Merriweather, Rubik, Roboto_Mono } from "next/font/google";
 
-import { useSearchParams } from 'next/navigation'
+import { useSearchParams } from "next/navigation";
 
 import { adjustBrightness } from "@/utils";
-
 
 const merriweather = Merriweather({
     subsets: ["latin"],
     weight: ["900"],
-})
+});
 
 const rubik = Rubik({
     subsets: ["latin"],
     weight: ["400"],
-})
+});
 
 const roboto_mono = Roboto_Mono({
     subsets: ["latin"],
     weight: ["400"],
-})
-
+});
 
 export default function Home() {
-    const searchParams = useSearchParams()
+    const searchParams = useSearchParams();
 
     return (
         <main className={rubik.className}>
-            <img
-                src={searchParams.get('image_url')}
-                className="w-full"
-            />
-            <div class="p-8 flex flex-col gap-16" style={{
-                background: adjustBrightness(searchParams.get('bg_color'), 150)
-            }}>
+            <img src={searchParams.get("image_url")} className="w-full" />
+            <div
+                class="p-8 flex flex-col gap-16 text-black/80"
+                style={{
+                    background: adjustBrightness(
+                        searchParams.get("bg_color"),
+                        170
+                    ),
+                }}
+            >
                 <div className="flex flex-col gap-2">
-                    <span className={"text-base text-black/90 " + roboto_mono.className}>{searchParams.get("season")}</span>
-                    <h1 className={"font-black text-5xl text-black/90 " + merriweather.className}>{searchParams.get("title")}</h1>
-                    <p className="text-2xl text-black/90">{searchParams.get("type")} - {searchParams.get("duration")} - {searchParams.get("status")}</p>
+                    <span className={"text-base " + roboto_mono.className}>
+                        {searchParams.get("season")}
+                    </span>
+                    <h1
+                        className={
+                            "font-black text-5xl " + merriweather.className
+                        }
+                    >
+                        {searchParams.get("title")}
+                    </h1>
+                    <p className="text-2xl">
+                        {searchParams.get("type")} -{" "}
+                        {searchParams.get("duration")} -{" "}
+                        {searchParams.get("status")}
+                    </p>
                 </div>
-                <div className="flex flex-row items-center gap-2 flex-wrap">
-                    {searchParams.get("genres").split(",").map((genre) => (
-                        <Genre title={genre} />
-                    ))}
+                <div className="flex flex-row items-center gap-4 flex-wrap">
+                    {searchParams
+                        .get("genres")
+                        .split(",")
+                        .map((genre) => (
+                            <Genre title={genre} />
+                        ))}
                 </div>
             </div>
         </main>
@@ -55,12 +71,8 @@ export default function Home() {
 }
 
 function Genre({ title }) {
-    const searchParams = useSearchParams()
-
     return (
-        <div className="p-2 rounded-lg leading-none text-black/90 text-xl" style={{
-            backgroundColor: adjustBrightness(searchParams.get('bg_color'), 130)
-        }}>
+        <div className="py-2 px-4 rounded-full border border-black/80 leading-none text-xl">
             {title}
         </div>
     );
--- anicard/src/app/cards/2/page.js
+++ anicard/src/app/cards/2/page.js
@@ -6,48 +6,67 @@ Vertical card with poster.
 
 import { Merriweather, Rubik, Roboto_Mono } from "next/font/google";
 
-import { useSearchParams } from 'next/navigation'
+import { useSearchParams } from "next/navigation";
 
 import { adjustBrightness } from "@/utils";
-
 
 const merriweather = Merriweather({
     subsets: ["latin"],
     weight: ["900"],
-})
+});
 
 const rubik = Rubik({
     subsets: ["latin"],
     weight: ["400"],
-})
+});
 
 const roboto_mono = Roboto_Mono({
     subsets: ["latin"],
     weight: ["400"],
-})
-
+});
 
 export default function Home() {
-    const searchParams = useSearchParams()
+    const searchParams = useSearchParams();
 
     return (
         <main className={rubik.className + " w-full flex flex-row"}>
             <img
-                src={searchParams.get('image_url')}
+                src={searchParams.get("image_url")}
                 className="h-full max-h-screen"
             />
-            <div class="p-8 flex flex-col flex-1 h-screen justify-between" style={{
-                background: adjustBrightness(searchParams.get('bg_color'), 170)
-            }}>
-                <div className="flex flex-col gap-2">
-                    <span className={"text-lg text-black/80 " + roboto_mono.className}>{searchParams.get("season")}</span>
-                    <h1 className={"font-black text-5xl text-black/80 " + merriweather.className}>{searchParams.get("title")}</h1>
-                    <p className="text-3xl text-black/80">{searchParams.get("type")} - {searchParams.get("duration")} - {searchParams.get("status")}</p>
+            <div
+                class="p-8 flex flex-col flex-1 h-screen justify-between"
+                style={{
+                    background: adjustBrightness(
+                        searchParams.get("bg_color"),
+                        170
+                    ),
+                }}
+            >
+                <div className="flex flex-col gap-2 text-black/80">
+                    <span className={"text-lg " + roboto_mono.className}>
+                        {searchParams.get("season")}
+                    </span>
+                    <h1
+                        className={
+                            "font-black text-5xl " + merriweather.className
+                        }
+                    >
+                        {searchParams.get("title")}
+                    </h1>
+                    <p className="text-3xl">
+                        {searchParams.get("type")} -{" "}
+                        {searchParams.get("duration")} -{" "}
+                        {searchParams.get("status")}
+                    </p>
                 </div>
                 <div className="flex flex-row items-center gap-4 flex-wrap">
-                    {searchParams.get("genres").split(",").map((genre) => (
-                        <Genre title={genre} />
-                    ))}
+                    {searchParams
+                        .get("genres")
+                        .split(",")
+                        .map((genre) => (
+                            <Genre title={genre} />
+                        ))}
                 </div>
             </div>
         </main>
@@ -55,10 +74,8 @@ export default function Home() {
 }
 
 function Genre({ title }) {
-    const searchParams = useSearchParams()
-
     return (
-        <div className="py-2 px-4 rounded-full border border-black/80 leading-none text-black/80 text-2xl">
+        <div className="py-2 px-4 rounded-full border border-black/80 leading-none text-2xl">
             {title}
         </div>
     );
